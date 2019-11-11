ec98f28edec55b50c03f429b2e5dbd8a058401f0
Alexander Kouznetsov <akouznetsov@hazeltree.com>
2019-11-11 11:58:35 +0300

    Visual Studio solution added and sources added to it. NameSpace changed to TestMailServer.Core. ConsoleApp for debug added.

---
+++ Tests/TestMailServer.UnitTests/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("TestMailServer.UnitTests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("TestMailServer.UnitTests")]
+[assembly: AssemblyCopyright("Copyright ©  2019")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("af45410b-078d-4fac-9969-fff78efc8053")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
